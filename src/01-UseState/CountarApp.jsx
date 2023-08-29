7f00eaa9631f9bf78ed6e028772120413aeb9a45
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-08-28 19:36:07 -0500

    Second example use state using an object

--- src/01-UseState/CountarApp.jsx
+++ src/01-UseState/CountarApp.jsx
@@ -1,18 +1,29 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const CountarApp = () => {
-    const [counter, setcounter] = useState(10)
-  return (
-    <div>
-      <h1>Couanter : {counter} </h1>
+    const [counter, setcounter] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+    });
 
-      <hr />
+    return (
+        <>
+            <h1>Couanter : {counter.counter1} </h1>
+            <h1>Couanter : {counter.counter2} </h1>
+            <h1>Couanter : {counter.counter3} </h1>
 
+            <hr />
+            <button
+                onClick={
+                    () => setcounter({
+                        counter1: counter.counter1 + 1,
+                        counter2: counter.counter2,
+                        counter3: counter.counter3,
+                    })
+                }> +1 </button>
+        </>
+    );
+};
 
-        <button onClick={() => setcounter(counter + 1) }> +1 </button>   
-
- </div>
-  )
-}
-
-export default CountarApp
+export default CountarApp;
