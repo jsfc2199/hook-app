9f80ca8ab30eeb0106b48940feb53d9487c3d001
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-09-25 16:00:30 -0500

    Added boostrap classes to make the structure prototype for the app

--- src/08-Reducer/TodoApp.jsx
+++ src/08-Reducer/TodoApp.jsx
@@ -11,19 +11,43 @@ const initialState = [
   {
     id: new Date().getTime() * 3,
     description: "Recolecatr prieda del alma",
+    done: false,
   },
 ];
 const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, initialState); //el reducer no se ejecuta es decur todoReducer() NO, solo se pasa la referecia
   return (
     <>
-      <h1>Todo App</h1>
+      <h1>
+        Todo App: 10, <small>pendientes: 2</small>
+      </h1>
       <hr />
-      <ul>
-        <li>itrem 1</li>
-        <li>itrem 2</li>
-        <li>itrem 3</li>
-      </ul>
+
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between">
+              <span className="align-self-center">itrem 1</span>
+              <button className="btn btn-danger">Borrar</button>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col-5">
+          <h4>Agregar Todo</h4>
+          <hr />
+          <form action="">
+            <input
+              type="text"
+              placeholder="que hay para hacer?"
+              className="formControl"
+            />
+            <button type="submit" className="btn btn-outline-primary mt-1">
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
