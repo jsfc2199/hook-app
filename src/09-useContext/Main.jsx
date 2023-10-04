1498451d61be34c3981f8f873be663fd0f82dc2d
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-10-04 12:31:38 -0500

    Route and route config

--- src/09-useContext/Main.jsx
+++ src/09-useContext/Main.jsx
@@ -1,10 +1,29 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import AboutPage from "./AboutPage";
+
 const MainApp = () => {
   return (
     <>
-    <h1>Main App</h1>
-    <hr />
-    </>
-  )
-}
+      <h1>Main App</h1>
+      <hr />
 
-export default MainApp
+      {/* routes es un arreglo de rutas de route */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="login" element={<LoginPage />} />
+
+        {/* para rutas que no existan podemos redirigirlo a donde queramos o mostrar una pagina de error */}
+        {/* <Route path="/*" element={<LoginPage />} /> */}
+
+        {/* la forma comun es usar navigate de ract router dom */}
+        <Route path="/*" element={<Navigate to={"/about"}/>} />
+
+      </Routes>
+    </>
+  );
+};
+
+export default MainApp;
